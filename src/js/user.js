0ec2d0e8dbf4583a4e3899bb64ab52c9f923fcc7
Dominik Magic <dominikmagic5@gmail.com>
2022-11-14 14:11:08 -0800

    test 19 for User() passed, it will display time is up message

--- src/js/user.js
+++ src/js/user.js
@@ -2,17 +2,17 @@ export default class User {
   constructor(age, averageLifeExpectancy) {
     this.earthAge = age;
     this.lifeExp = averageLifeExpectancy;
-  }
+  };
 
   remainingLifeEarth() {
     let remainingLifeEarth = parseInt(this.lifeExp - this.earthAge)
     return remainingLifeEarth
-  }
+  };
 
   mercuryAge() {    
     this.mercuryAge = parseInt(this.earthAge/0.24);
     return this.mercuryAge;
-  }
+  };
 
   remainingLifeMercury() {  
     // always positive number, maybe differentiate with if statement if life exceeded
@@ -25,12 +25,12 @@ export default class User {
       remainingLifeMercury = 'You have 0 years left, your time is up!'
     }
     return remainingLifeMercury
-  }
+  };
 
   venusAge() {    
     this.venusAge = parseInt(this.earthAge/0.62);
     return this.venusAge;
-  }
+  };
 
   remainingLifeVenus() {
     let remainingLifeVenus;
@@ -38,17 +38,21 @@ export default class User {
     remainingLifeVenus = parseInt(this.remainingLifeEarth()/0.62);
     } else if (this.lifeExp < this.earthAge) {
     remainingLifeVenus = Math.abs(parseInt(this.remainingLifeEarth()/0.62));  
+    } else {
+      remainingLifeVenus = 'You have 0 years left, your time is up!'
     }
     return remainingLifeVenus
-  }
+  };
 
   marsAge() {    
     this.marsAge = parseInt(this.earthAge/1.88);
     return this.marsAge;
-  }
+  };
 
   jupiterAge() {
     this.jupiterAge = parseInt(this.earthAge/11.86);
     return this.jupiterAge;
-  }
+  };
+
+  
 }
