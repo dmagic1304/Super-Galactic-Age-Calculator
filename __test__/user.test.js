0d1cc96e7e4e9a19139ece82241a42da4493bbae
Dominik Magic <dominikmagic5@gmail.com>
2022-11-14 08:51:54 -0800

    created beforeEach for User() testing to create new User

--- __test__/user.test.js
+++ __test__/user.test.js
@@ -2,8 +2,15 @@ import User from './../src/js/user.js'
 
 describe('User', () => {
 
+  beforeEach(() => {
+    let newUser = new User (30);
+  });
+
   test('1. It will create a new user with current age property', () => {
-    const newUser = new User(30);
-    expect(newUser.currentAge).toEqual(30);
+   expect(newUser.currentAge).toEqual(30);
   })
+
+  test('2. It will give the user a mercuryAge property with the calculated value'), () => {
+
+  }
 })
