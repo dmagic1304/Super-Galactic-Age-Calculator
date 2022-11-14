2446b19860c2178b21536ebcb6dbe59656f05f6c
Dominik Magic <dominikmagic5@gmail.com>
2022-11-14 08:33:49 -0800

    Test 1 for User() passed

--- __test__/user.test.js
+++ __test__/user.test.js
@@ -1,9 +1,9 @@
-
+import User from './../src/js/user.js'
 
 describe('User', () => {
 
   test('1. It will create a new user with current age property', () => {
     const newUser = new User(30);
-    expect(newUser.currentAge).toEqueal(30);
+    expect(newUser.currentAge).toEqual(30);
   })
 })
